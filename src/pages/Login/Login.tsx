0b0005d296a528186c51f9fcd84db5b0b1ce4c6a
RamonCosta07 <ramonnar20@hotmail.com>
2023-05-15 22:05:22 -0300

    Melhorada a interface da página de Login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,7 +2,10 @@
 import { auth, provider } from "../../services/firebase";
 import { signInWithPopup } from "firebase/auth";
 // Styles
-import { Container, Button } from "./LoginStyles";
+import * as S from "./LoginStyles";
+// Icon
+import { FaGoogle } from "react-icons/fa";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Login = () => {
   const handleSignIn = async () => {
@@ -16,9 +19,21 @@ const Login = () => {
   };
 
   return (
-    <Container>
-      <Button onClick={handleSignIn}>Login com Google</Button>
-    </Container>
+    <>
+      <S.Container>
+        <S.Title>
+          Bem-vindo ao ChatApp
+          <BsWhatsapp />
+        </S.Title>
+        <S.Information>
+          Faça login com sua conta Google para começar a usar
+        </S.Information>
+        <S.Button onClick={handleSignIn}>
+          <FaGoogle />
+          Login com Google
+        </S.Button>
+      </S.Container>
+    </>
   );
 };
 
--- src/pages/Login/LoginStyles.tsx
+++ src/pages/Login/LoginStyles.tsx
@@ -1,15 +1,50 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f5f5f5; /* Cor de fundo */
+  font-family: Arial, sans-serif; /* Fonte */
+`;
 
 export const Button = styled.button`
-    outline: none;
-    font-size: 18px;
-    padding: 14px 18px;
-    cursor: pointer;
-`
+  outline: none;
+  font-size: 18px;
+  padding: 14px 18px;
+  cursor: pointer;
+  background-color: #4285f4; /* Cor de fundo do botão */
+  color: #fff; /* Cor do texto */
+  border: none;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #3367d6; /* Cor de fundo ao passar o mouse */
+  }
+
+  & svg{
+    margin-right: 1rem;
+    margin-top: 5px;
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 24px;
+  margin-bottom: 20px;
+
+  & svg{
+    margin-left: .5rem;
+  }
+`;
+
+export const Information = styled.p`
+  font-size: 16px;
+  font-weight: bold;
+  color: #4285f4;
+  margin-bottom: 1.5rem;
+  margin-top: -.5rem;
+`;
